f2050788c983cb43a8e5087daa1487e2cebed62d
Vitor Ferreira <victorsrf010@gmail.com>
2023-05-28 11:56:51 +0100

    login completo

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+IonicStorageModule.forRoot({
+  driverOrder: [
+    CordovaSQLiteDriver._driver,
+    Drivers.IndexedDB,
+    Drivers.LocalStorage
+  ],
+});
