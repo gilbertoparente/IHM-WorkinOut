d1abc4a26a3e54492fdf99f1c37e195ffd4dce41
gilbertoparente <80535077+gilbertoparente@users.noreply.github.com>
2023-05-30 14:03:13 +0100

    feed, treinar, perfil

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+interface FeedItem {
+  title: string;
+  description: string;
+  image: string;
+  author: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-feed',
@@ -7,25 +14,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
+  public publicacoes: FeedItem[] = [
+    {
+      title: 'Metas de rendimento',
+      description: 'Mr. Sports aconcelha as seguintes metas',
+      image: '../../assets/feedtreino1.png',
+      author: 'Mr. Sports',
+      date: '2023-06-25'
+    },
+    {
+      title: 'Desporto e saúde',
+      description: 'Desporto e saúde para todos',
+      image: '../../assets/feedtreino3.png',
+      author: 'Mr. Sports',
+      date: '2023-02-25'
+    },
+    {
+      title: 'Desporto',
+      description: '',
+      image: '../../assets/feedtreino4.png',
+      author: 'Mr. Sports',
+      date: '2023-05-25'
+    },
+    {
+      title: 'Fitness Women',
+      description: 'Descrição  2',
+      image: '../../assets/feedtreino2.png',
+      author: 'Mrs. Lady',
+      date: '2022-05-24'
+    }
+    // Adicione mais itens ao feedData conforme necessário
+  ];
 
-  public publicacoes!: any[];
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ionViewDidEnter() {
-    this.carregarPublicacoes();
-  }
-
-  carregarPublicacoes() {
-    const url = 'https://api.example.com/feed'; // Substitua pela URL da API real
-
-    this.http.get<any[]>(url).subscribe(data => {
-      this.publicacoes = data;
-    }, error => {
-      console.log('Erro ao carregar as publicações', error);
-    });
   }
 }
--- src/app/treinar/treinar.page.ts
+++ src/app/treinar/treinar.page.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
+
+interface Planos {
+  menu: string;
+  categoria: string;
+  subCategoria: string;
+  nome: string;
+  duracao: string;
+  exercicios: string[];
+}
 
 @Component({
   selector: 'app-treinar',
@@ -6,10 +18,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./treinar.page.scss'],
 })
 export class TreinarPage implements OnInit {
+  public planos: Planos[] = [];
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    fetch('./assets/planos.json')
+      .then(res => res.json())
+      .then(data => {
+        this.planos = data.planos;
+      })
+      .catch(error => {
+        console.error('Erro ao carregar o JSON:', error);
+      });
   }
 
+  getVideoUrl(categoria: string): SafeResourceUrl {
+    if (categoria === 'Alongamentos') {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/4-4f10fL1pw');
+    } else if (categoria === 'Category 1') {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/q2NZyW5EP5A');
+    } else if (categoria === 'Category 2') {
+      return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/q2NZyW5EP5A');
+    }    
+    else {
+      return '';
+    }
+  }
+  
 }
